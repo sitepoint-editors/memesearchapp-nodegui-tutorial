5717b174dd94fd78e58148ddb45a68bc8bf55e80
Atul R <atulanand94@gmail.com>
2020-01-27 21:19:57 +0100

    Adds basic animation example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-const { QMainWindow, QLabel } = require("@nodegui/nodegui");
+const { QMainWindow, QMovie, QLabel } = require("@nodegui/nodegui");
+const axios = require("axios").default;
 
-const win = new QMainWindow();
-win.setWindowTitle("Meme Search");
+const main = async () => {
+  const win = new QMainWindow();
+  win.setWindowTitle("Meme Search");
 
-const label = new QLabel();
-label.setText("Hello World");
+  const label = new QLabel();
+  label.setText("Hello World");
 
-win.setCentralWidget(label);
-win.show();
+  const gifMovie = await getMovie(
+    "https://upload.wikimedia.org/wikipedia/commons/e/e3/Animhorse.gif"
+  );
+  label.setMovie(gifMovie);
 
-global.win = win;
+  win.setCentralWidget(label);
+  win.show();
+
+  global.win = win;
+};
+
+async function getMovie(url) {
+  const { data } = await axios.get(url, { responseType: "arraybuffer" });
+  const movie = new QMovie();
+  movie.loadFromData(data);
+  movie.start();
+  return movie;
+}
+
+main().catch(console.error);
